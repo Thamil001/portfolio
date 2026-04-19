f81b8d1038c53822a57cd6d6dd81177d78d30fcd
Thamil001 <thamilthennarasu@gmail.com>
2026-04-19 17:08:09 +0530

    improve logo animation style

--- src/assets/animation/logo.jsx
+++ src/assets/animation/logo.jsx
@@ -1,14 +1,39 @@
-import {animation} from "../../data/data.js"
+import { forwardRef, useRef, useImperativeHandle, useEffect } from "react";
+import { animation } from "../../data/data.js";
 
-export default function Logo() {
+const Logo = forwardRef((props, ref) => {
+  const videoRef = useRef(null);
+  const playedOnce = useRef(false);
+
+  useImperativeHandle(ref, () => ({
+    play: () => {
+      const video = videoRef.current;
+      video.currentTime = 0;
+      video.play();
+    }
+  }));
+
+  // autoplay only first time
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (!playedOnce.current) {
+      video.currentTime = 0;
+      video.play();
+      playedOnce.current = true;
+    }
+  }, []);
+
   return (
     <video
+      ref={videoRef}
       src={animation.url}
-      autoPlay
       muted
       playsInline
       preload="auto"
       style={{ width: "60px", height: "60px" }}
     />
   );
-}
+});
+
+export default Logo;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,78 +6,91 @@ import ContactPage from "../Contacts/ContactPage";
 import MyProjectsPage from "../MyProjectsPage/MyProjectsPage";
 import About from "../About/About"
 import NavList from "./components/NavList"
-import { useState,useRef,useEffect } from "react"
+import { useState, useRef, useEffect } from "react"
 import Logo from "../../assets/animation/logo"
 export default function HomePage() {
     const ref = useRef(null);
+    const videoRef = useRef();
     const [animate, setAnimate] = useState(false);
+    const [logoAnimation, setLogoAnimation] = useState(true);
+    const handleClick = () => {
+        videoRef.current.currentTime = 0;
+        videoRef.current.play();
+        setLogoAnimation(false);
+
+        setTimeout(() => {
+            setLogoAnimation(true);
+        }, 2000);
+    };
     useEffect(() => {
         const observer = new IntersectionObserver(
-            ([entry])=> {
+            ([entry]) => {
                 setAnimate(entry.isIntersecting)
             },
-            {threshold:0.6}
+            { threshold: 0.6 }
         );
         observer.observe(ref.current);
-        return ()=> observer.disconnect();
+        return () => observer.disconnect();
     }, []);
 
     const [open, setOpen] = useState(false)
     const menuBar = () => {
-       setOpen(prev=>!prev) 
-    } 
-    const [active,setActive] = useState("home")
-    const navList = [{ "name":"Home","destination":"home"},{"name":"Skill Set","destination":"skillSet"},{"name":"Projects","destination":"projects"},{"name":"Contacts","destination":"contacts"},{"name":"About","destination":"about"}]
+        setOpen(prev => !prev)
+    }
+    const [active, setActive] = useState("home")
+    const navList = [{ "name": "Home", "destination": "home" }, { "name": "Skill Set", "destination": "skillSet" }, { "name": "Projects", "destination": "projects" }, { "name": "Contacts", "destination": "contacts" }, { "name": "About", "destination": "about" }]
     return (
         <div className="overflow-y-scroll scrollbar-hide bg-cover bg-center min-h-screen scroll-smooth px-0.5" style={{ background: `url(${bg})` }}>
-            <div className="fixed left-1 top-1">
-                    <Logo/>
+            <div onClick={handleClick} className={`cursor-pointer fixed left-1 top-1 z-4 ${logoAnimation ? "animate-ping-once" : ""}`}>
+                <Logo ref={videoRef} />
             </div>
             <div ref={ref} className={`fixed top-4 right-2 z-2 lg:hidden animate-spin [animation-iteration-count:1]`}>
                 {
-                    !open?<img src={menu} alt="menu" className="w-6 h-6" onClick={menuBar} />:<img src={close} alt="close" className="w-5 h-5 animate-spin [animation-iteration-count:0.5]" onClick={menuBar} />
+                    !open ? <img src={menu} alt="menu" className="w-6 h-6" onClick={menuBar} /> : <img src={close} alt="close" className="w-5 h-5 animate-spin [animation-iteration-count:0.5]" onClick={menuBar} />
                 }
             </div>
             <div className="hidden lg:flex flex-row z-3 fixed top-0 w-full justify-end items-center  p-6 text-white">
                 <ul className="flex gap-6 font-medium" style={{ "fontFamily": "Poppins" }}>
                     {
-                        navList.map((list,index)=>{ 
-                            return(
-                            <NavList key={index} onClick={menuBar} name={list.name} destination={list.destination} active={active} setActive={setActive}/>
-                        )})
+                        navList.map((list, index) => {
+                            return (
+                                <NavList key={index} onClick={menuBar} name={list.name} destination={list.destination} active={active} setActive={setActive} />
+                            )
+                        })
                     }
                 </ul>
             </div>
             <div className={`flex flex-row justify-center fixed z-1 top-0 right-0 h-full w-12/12 bg-black shadow-lg transform transition-transform duration-300 ${open ? "translate-x-0" : "translate-x-full"} lg:hidden`}>
                 <ul className="p-12 space-y-6 text-white " style={{ "fontFamily": "Poppins" }}>
                     {
-                        navList.map((list,index)=>{ 
-                            return(
-                            <NavList key={index} onClick={menuBar} name={list.name} destination={list.destination} active={active} setActive={setActive}/>
-                        )})
+                        navList.map((list, index) => {
+                            return (
+                                <NavList key={index} onClick={menuBar} name={list.name} destination={list.destination} active={active} setActive={setActive} />
+                            )
+                        })
                     }
                 </ul>
             </div>
-            <div id="home" ref={ref} className={`flex flex-col h-screen justify-center items-center gap-2 ${animate?"animate-slideUp [animation-delay:0.2s]":""}`}>
+            <div id="home" ref={ref} className={`flex flex-col h-screen justify-center items-center gap-2 ${animate ? "animate-slideUp [animation-delay:0.2s]" : ""}`}>
                 <span className="inline-block text-white font-bold text-4xl" style={{ "fontFamily": "Inter" }}>Who Am I?</span>
                 <span className="text-white font-normal text-2xl text-center" style={{ "fontFamily": "Poppins" }}>Hi, I'm <span className="text-yellow-400">Thamilthennarasu </span>Full Stack Developer.</span>
             </div>
-            
+
             <div id="skillSet">
-                <SkillSetPage/>
+                <SkillSetPage />
             </div>
 
 
             <div id="projects">
-                <MyProjectsPage/> 
+                <MyProjectsPage />
             </div>
 
             <div id="contacts">
-                <ContactPage/>
+                <ContactPage />
             </div>
 
             <div id="about">
-                <About/> 
+                <About />
             </div>
 
         </div>
